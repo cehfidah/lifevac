faf7e9b157ecbf934b7a3c7b044b25c2807e5bbb
Doshi Kirtan <133971416+DoshiKirtan@users.noreply.github.com>
2025-05-15 11:06:11 +0530

    OfferSelection componet design change

--- src/pages/home/component/OfferSelection.js
+++ src/pages/home/component/OfferSelection.js
@@ -28,31 +28,40 @@ const OfferSelection = () => {
     const offers = [
         {
             id: 'offer1',
-            title: 'BUY ONE',
-            kits: '1x Full Kit',
-            price: 3448.67,
-            originalPrice: 6100.00,
-            savings: 2651.33,
+            image: "https://cdn.shopify.com/s/files/1/0638/0378/5380/files/Kaching-Bundles-Adult_Mask_3_300x300.svg?v=1737985220",
+            sectionTitle: "PROTECTION FOR YOURSELF AND YOUR KID:",
+            title: "BUY ONE",
+            kits: "1× Full Kit",
+            price: 3474.64,
+            originalPrice: 6100,
+            savings: 2625.36,
             guideIncluded: false,
-            image: "https://cdn.shopify.com/s/files/1/0638/0378/5380/files/Kaching-Bundles-Adult_Mask_3_300x300.svg?v=1737985220"
+            tag: false,
+            tagText: "",
         },
         {
             id: 'offer2',
-            title: 'BUY 2 GET 1 FREE',
-            kits: '3x Full Kits',
-            price: 6897.34,
-            originalPrice: 18300.00,
-            savings: 11402.66,
+            sectionTitle: "PROTECTION FOR YOUR CLOSE FAMILY:",
+            title: "BUY 2 GET 1 FREE",
+            kits: "3× Full Kits",
+            price: 6949.27,
+            originalPrice: 18300,
+            savings: 11350.73,
+            tag: true,
+            tagText: "MOST POPULAR",
             guideIncluded: true,
             image: "https://cdn.shopify.com/s/files/1/0638/0378/5380/files/Kaching-Bundles-Adult_Mask_2_300x300.svg?v=1737985180"
         },
         {
             id: 'offer3',
-            title: 'BUY 3 GET 2 FREE',
-            kits: '5x Full Kits',
-            price: 8621.24,
-            originalPrice: 30500.00,
-            savings: 21878.76,
+            sectionTitle: "PROTECTION FOR YOUR LARGE FAMILY:",
+            title: "BUY 3 GET 2 FREE",
+            kits: "5× Full Kits",
+            price: 8686.16,
+            originalPrice: 30500,
+            savings: 21813.84,
+            tag: true,
+            tagText: "MOST SAVINGS",
             guideIncluded: true,
             image: "https://cdn.shopify.com/s/files/1/0638/0378/5380/files/Kaching-Bundles-Adult_Mask_4_300x300.svg?v=1737985322"
         },
@@ -65,40 +74,61 @@ const OfferSelection = () => {
                 {offers.map((offer, idx) => (
                     <div
                         key={offer.id}
-                        onClick={() => handleSelectOffer(offer)}
-                        className={`border rounded-xl p-4 text-left shadow-md mb-6 cursor-pointer ${selectedId === offer.id ? 'border-blue-900 bg-blue-50' : 'hover:border-blue-400'
-                            }`}
+                        className="relative"
                     >
-                        <h4 className="text-sm font-bold text-blue-900 mb-2 uppercase">
+                        <h4 className="text-sm font-bold text-blue-900 mb-2 uppercase text-start">
                             {idx === 0
                                 ? 'Protection For Yourself And Your Kid:'
                                 : idx === 1
                                     ? 'Protection For Your Close Family:'
                                     : 'Protection For Your Large Family:'}
                         </h4>
-
-                        <div className="flex items-center justify-between mb-1">
-                            <span className="font-bold text-lg">{offer.title}</span>
-                            <span className="font-semibold text-gray-800">Rs. {offer.price}</span>
-                        </div>
-
-                        <p className="text-sm text-gray-600 mb-1">{offer.kits}</p>
-                        <p className="text-sm line-through text-gray-400">Rs. {offer.originalPrice}</p>
-
-                        <p className="bg-blue-900 text-white text-xs px-2 py-1 inline-block rounded mt-2">
-                            You Save Rs. {offer.savings}
-                        </p>
-
-                        {offer.guideIncluded && (
-                            <div className="flex justify-between items-center bg-[#001532] px-2 py-2 mt-2">
-                                <p className="text-sm mt-1 text-white">
-                                    + <strong>FREE Home Medic Guide</strong>
-                                </p>
-                                <p className="bg-blue-900 text-white text-xs px-2 py-1 inline-block rounded">
-                                    Free Bonus
-                                </p>
+                        {offer.tag && (
+                            <div className="flex justify-end pr-1">
+                                <div className=" bg-blue-900 text-white text-xs font-bold px-2 py-1 rounded-tl-md rounded-tr-md">
+                                    {offer.tagText}
+                                </div>
                             </div>
                         )}
+                        <div
+                            onClick={() => handleSelectOffer(offer)}
+                            className={`border rounded-xl text-left shadow-md mb-6 cursor-pointer ${selectedId === offer.id ? 'border-blue-900 bg-blue-50' : 'hover:border-blue-400'
+                                }`}
+                        >
+                            <div className="flex justify-between items-start gap-4 p-4">
+                                <div className="flex items-center justify-center">
+                                    <img src={offer.image} alt="Kit" className="w-16 h-16 object-contain" />
+                                </div>
+                                <div>
+                                    <span className="font-bold text-xl">{offer.title}</span>
+                                    <div className="text-sm text-gray-600 mb-1">
+                                        {offer.kits}
+                                    </div>
+                                </div>
+                                <div>
+                                    <span className="font-semibold text-gray-800">Rs. {offer.price.toLocaleString("en-IN")}</span>
+                                    <div className="text-sm line-through text-gray-400">
+                                        Rs. {offer.originalPrice.toLocaleString("en-IN")}
+                                    </div>
+                                    {!offer.guideIncluded && (
+                                        <p className="bg-blue-900 text-white text-xs px-2 py-1 inline-block rounded mt-2">
+                                            You Save Rs. {offer.savings}
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+
+                            {offer.guideIncluded && (
+                                <div className={`${offer.id === "offer2" ? "bg-[#001532] text-white" : "bg-[#0015324d] text-black"} flex justify-between items-center px-2 py-2 mt-2 rounded-br-xl rounded-bl-xl`}>
+                                    <p className="text-sm mt-1">
+                                        + <strong>FREE Home Medic Guide</strong>
+                                    </p>
+                                    <p className="bg-blue-900 text-white text-xs px-2 py-1 inline-block rounded">
+                                        You Save Rs. {offer.savings}
+                                    </p>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 ))}
 
@@ -149,4 +179,4 @@ const OfferSelection = () => {
     )
 }
 
-export default OfferSelection
+export default OfferSelection;
