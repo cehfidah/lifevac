6c452fcea4099b77ffd417e569ba825f013404f8
brij_r_24 <brijp6386@gmail.com>
2025-05-19 18:11:53 +0530

    update logo

--- src/components/dashboardlayout/Header.js
+++ src/components/dashboardlayout/Header.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Container from "../Container";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slice/authSlice";
+import logo from "../../assest/logo2.avif";
 
 export default function Header() {
   const [openDropdown, setOpenDropdown] = useState(false);
@@ -16,15 +17,21 @@ export default function Header() {
     navigate("/");
   };
 
-
   return (
     <Container>
       <header className="flex justify-between items-center paddingX py-2 md:py-4 bg-white">
         <div className="flex items-center space-x-8">
-          <Link to="/" className="text-lg font-bold">AirwayClear</Link>
+          <Link to="/" className="text-lg font-bold">
+            {" "}
+            <img width={200} src={logo} />
+          </Link>
           <ul className="flex space-x-2 text-gray-700 font-medium">
-            <li className="hover:bg-[#f5f5f5] p-2 md:p-4 hover:rounded-xl"><Link to="/">Shop</Link></li>
-            <li className="hover:bg-[#f5f5f5] p-2 md:p-4 hover:rounded-xl"><Link to="/orders">Orders</Link></li>
+            <li className="hover:bg-[#f5f5f5] p-2 md:p-4 hover:rounded-xl">
+              <Link to="/">Shop</Link>
+            </li>
+            <li className="hover:bg-[#f5f5f5] p-2 md:p-4 hover:rounded-xl">
+              <Link to="/orders">Orders</Link>
+            </li>
           </ul>
         </div>
         <div className="relative">
@@ -36,9 +43,22 @@ export default function Header() {
           </button>
           {openDropdown && (
             <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-md py-2 z-20">
-              <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
-              <Link to="/settings" className="block px-4 py-2 hover:bg-gray-100">Settings</Link>
-              <button type="button" onClick={handleLogout} className="block w-full text-left px-4 py-2 hover:bg-gray-100">Logout</button>
+              <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
+                Profile
+              </Link>
+              <Link
+                to="/settings"
+                className="block px-4 py-2 hover:bg-gray-100"
+              >
+                Settings
+              </Link>
+              <button
+                type="button"
+                onClick={handleLogout}
+                className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+              >
+                Logout
+              </button>
             </div>
           )}
         </div>
