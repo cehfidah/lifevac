b5c25d7eb70443e6a1e29b0f851f6b33af1ef8b5
cehfidah <140584828+cehfidah@users.noreply.github.com>
2025-08-20 00:56:24 +0100

    fix issue fhj

--- src/pages/checkouts/Checkouts.js
+++ src/pages/checkouts/Checkouts.js
@@ -507,7 +507,7 @@ useEffect(() => {
 
       {/* Modal */}
       {showModal && (
-        <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-start justify-center overflow-y-auto pt-10 pb-10">
           <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative">
             {/* Top Close Button */}
             <button
